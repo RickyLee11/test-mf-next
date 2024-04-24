ac492d07fb1392ff441ebd2bec4df41de6b613f9
Ricky <ricky101305324@gmail.com>
2024-04-24 23:23:40 +0800

    add zustand and store on host

--- host/pages/index.js
+++ host/pages/index.js
@@ -1,5 +1,13 @@
+import useStore from "../zustand/store";
+
 export default function App() {
+    const [count, increment, clear] = useStore((state) => [state.count, state.increment, state.clear]);
     return (
+    <>
         <div>i am host</div>
+        <div>count: host {count}</div>
+        <button onClick={increment}>host increment</button>
+        <button onClick={clear}>host clear</button>
+    </>
     )
   }
